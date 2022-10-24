6c6e140ccbf557a2d32b48240974c3e83d9ca317
Agenor Torres <agenorpoker@gmail.com>
2022-10-24 11:09:35 -0300

    modificado o endereço das imagens

--- src/dados/index.jsx
+++ src/dados/index.jsx
@@ -1,100 +1,61 @@
 const listProduct = [
     {
         id: 1,
-        title: 'Combo Açai Ovomaltine em dobro',
-        description: ``,
-        itens: `02 deliciosos Açaí de 500ml
-         O dobro de Ovomaltine (em cada pote 2)
-         leite em pó
-         biscoito Negresco (triturado),
-         banana
-         Calda de morango`,
-        img: 'https://media-cdn.tripadvisor.com/media/photo-s/0e/15/4b/fc/acai-com-cobertura-de.jpg',
+        title: 'AQUI FICARÁ O NOME DO PRODUTO',
+        description: `Aqui ficará a descrição do seu produto`,
+        itens: ``,
+        img: 'https://f004.backblazeb2.com/file/NinAcai/ed_macrame/suspensoRosa1.jpeg',
         value: 48.90
 
     }
     ,
     {
         id: 2,
-        title: '350ML DE AÇAÍ E SORVETE.',
-        description: `Monte seu açaí com até:`,
+        title: 'AQUI FICARÁ O NOME DO PRODUTO',
+        description: `Aqui ficará a descrição do seu produto`,
         itens: `
-        04 Deliciosos complementos
-        01 Frutas
-        01 sorvete
-        02 Caldas`,
-        img: 'https://static-images.ifood.com.br/image/upload/t_medium/pratos/f4343035-ee36-4711-9572-606f0958c0ab/202207132344_7232_i.jpg',
+        `,
+        img: 'https://f004.backblazeb2.com/file/NinAcai/ed_macrame/suspensoHome2.jpeg',
         value: 33.90
     },
     {
         id: 3,
-        title: 'COMBO ESTAMOS COM FOME',
-        description: ``,
-        itens: `02 pote de açaí 750 ml
-        02 Leite em pó
-        02 Paçoca
-        02 Ovomaltine
-        02 Nutella
-        02 Banana
-        02 Calda de morango`,
-        img: 'https://static-images.ifood.com.br/image/upload/t_medium/pratos/f4343035-ee36-4711-9572-606f0958c0ab/202201081227_75DK_i.jpg',
+        title: 'AQUI FICARÁ O NOME DO PRODUTO',
+        description: `Aqui ficará a descrição do seu produto`,
+        itens: ``,
+        img: 'https://f004.backblazeb2.com/file/NinAcai/ed_macrame/suspensoDecoracao3.jpeg',
         value: 71.90
     },
     {
         id: 4,
-        title: 'AÇAI AMO AMENDOIM ',
-        description: `Está pronto para essa delícia dos derivados de amendoim!?? Então vem provar essa deliciosa mistura que inclui Paçoca, Amendoim, Xerém de amendoim, Farinha de amendoim para começar ... Também faz parte nossa Farinha de biscoito ao leite e Leite em pó e não esquece da banana. Ainda pode escolher até 3 caldas.
-        Não é Demais !?`,
-        itens: `01 açaí de 250ml com banana
-        Paçoca
-        Amendoim
-        Xerém de amendoim
-        Farinha de amendoim
-        Leite em pó`,
-        img: 'https://static-images.ifood.com.br/image/upload/t_medium/pratos/f4343035-ee36-4711-9572-606f0958c0ab/202207121752_0358_i.jpg',
+        title: 'AQUI FICARÁ O NOME DO PRODUTO',
+        description: `Aqui ficará a descrição do seu produto`,
+        itens: ``,
+        img: 'https://f004.backblazeb2.com/file/NinAcai/ed_macrame/suspensoRosa7.jpeg',
         value: 19.90
     },
     {
         id: 5,
-        title: 'AÇAÍ LOUCURA LOUCURA',
-        description: `Deu a louca nos complementos!!!
-        Que tal pegar nosso delicioso Açaí com Banana juntar com Leite em pó mais a Farinha de Biscoito ao Leite a Paçoca a Farinha de Castanha com a Jujuba, Aveia e Granola Tradicional. Há poder escolher até 3 caldas, ok!?
-        
-        Hum que delícia ficou eim ...`,
-        itens: `01 açaí de 150ml com banana
-        Leite em pó
-        Paçoca
-        Farinha de castanha
-        Jujuba
-        Granola Tradicional`,
-        img: 'https://static-images.ifood.com.br/image/upload/t_medium/pratos/f4343035-ee36-4711-9572-606f0958c0ab/202201081244_GT24_i.jpg',
+        title: 'AQUI FICARÁ O NOME DO PRODUTO',
+        description: `Aqui ficará a descrição do seu produto`,
+        itens: ``,
+        img: 'https://f004.backblazeb2.com/file/NinAcai/ed_macrame/suspensoRosa6.jpeg',
         value: 13.90
     },
     {
         id: 6,
-        title: 'COMBO CASAL',
-        description: `Para um casal que quer pegar leve.
-
-        Venha se deliciar com um Açaí coberto por banana e Granola tradicional, polvilhado com leite em pó e Paçoca. Tudo isso coberto por leite condensado.
-        
-        Hummm de dar água na boca`,
-        itens: `02 recipientes com 350 ml de açaí cada;
-        02 Bananas;
-        02 Leite em pó ;
-        02 Granola Tradicional;
-        02 Paçoca;
-        02 Calda de morango`,
-        img: 'https://static-images.ifood.com.br/image/upload/t_medium/pratos/f4343035-ee36-4711-9572-606f0958c0ab/202207121114_K561_i.jpg',
+        title: 'AQUI FICARÁ O NOME DO PRODUTO',
+        description: `Aqui ficará a descrição do seu produto`,
+        itens: ``,
+        img: 'https://f004.backblazeb2.com/file/NinAcai/ed_macrame/conjuntoPratos6.jpeg',
         value: 38.90
     },
     {
         id: 7,
-        title: 'FRUTAS COM AÇAÍ ',
-        description: `Monte seu açaí escolhendo até`,
-        itens: `03 complementos
-        02 frutas
-        02 caldas.`,
-        img: 'https://static-images.ifood.com.br/image/upload/t_medium/pratos/f4343035-ee36-4711-9572-606f0958c0ab/202108232020_8S0C_i.jpg',
+        title: 'AQUI FICARÁ O NOME DO PRODUTO',
+        description: `Aqui ficará a descrição do seu produto`,
+        itens: ``,
+        img: 'https://f004.backblazeb2.com/file/NinAcai/ed_macrame/conjuntoPratos5.jpeg',
         value: 30.90
     },
 ]
