5dc981e9f05b5c8c313f5298f7edff12018704d4
Ag <agenortorres10@gmail.com>
2024-10-19 15:46:50 -0300

    segundo commit

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import Carrossel from './Carrosel';
 import './style.css';
 
+
 function Products() {
+
     const [products] = useState([
         {
             title: "Gestão de Pedidos",
-            image: "url_da_imagem1.jpg",
+            image: "https://th.bing.com/th/id/OIG4.RYl2JWwUGNyY36Er21IK?w=1024&h=1024&rs=1&pid=ImgDetMain",
             description: "Gerenciamento eficiente dos pedidos, garantindo agilidade e precisão."
         },
         {
-            title: "Relatórios Detalhados",
-            image: "url_da_imagem2.jpg",
-            description: "Relatórios completos sobre vendas, marketing e produtos."
+            "title": "Relatórios Detalhados",
+            "image": "https://th.bing.com/th/id/OIG4.cTzMcsEs2sWoi4wjS4y9?pid=ImgGn",
+            "description": "A GESTÃO DE IFOOD É UMA PRESTAÇÃO DE SERVIÇO FOCADO EM ALAVANCAR SUAS VENDAS NO IFOOD, ONDE USAMOS AS ESTRATEGIAS NECESSÁRIAS PARA FAZER SEU RESTAURANTE VENDER MAIS E VENDER COM LUCRO!."
         },
         {
             title: "Suporte 24/7",
-            image: "url_da_imagem3.jpg",
+            image: "https://th.bing.com/th/id/OIG4.M23.KcT75BeI7YCml_K_?pid=ImgGn",
             description: "Suporte contínuo para resolver qualquer problema rapidamente."
         }
     ]);
 
     return (
         <section className="products">
-            <h2>Nossos Produtos</h2>
+            <h2>Nossos Serviços</h2>
             <Carrossel products={products} />
         </section>
     );
